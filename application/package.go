d8670564bc14c2968552bfe2c57b255c105bff77
Kaiyan M. Lee <108332894+kaiyanlee@users.noreply.github.com>
2024-07-20 08:34:26 -0400

    Update copyright date

--- application/package.go
+++ application/package.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 Sumartian Studios
+// Copyright (c) 2022-2024 Sumartian Studios
 //
 // Snake is free software: you can redistribute it and/or modify it under the
 // terms of the MIT license.
